5ad68168a793adc30d8837b817fa09f916b6dfd4
Diane ADONSOU <didiofficiall96@gmail.com>
2025-08-11 11:46:40 +0000

    Next

---
+++ src/app/a_propos/page.tsx
@@ -0,0 +1,12 @@
+export default function Apropos() {
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-center text-slate-900 text-4xl my-6">
+        PAGE A PROPOS
+      </h1>
+    </div>
+
+  )
+
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Page() {
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <h1 className="text-center text-slate-900 text-4xl my-6">
+        PAGE ACCUEIL
+      </h1>
+    </div>
+
+  )
+
+}
